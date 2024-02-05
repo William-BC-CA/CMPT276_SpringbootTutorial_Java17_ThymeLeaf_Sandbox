ccd6cfc8d783e0b10129091d6573d1b2b61dc734
ENTPRESTIGIOUS <bandcomposing54@gmail.com>
2024-02-05 13:30:45 -0800

    add new bus

--- src/main/java/com/springboot/initializer/java17/demo/controllers/BusController.java
+++ src/main/java/com/springboot/initializer/java17/demo/controllers/BusController.java
@@ -17,6 +17,7 @@ public class BusController {
         List<Bus> bus = new ArrayList<>();
         bus.add(new Bus("New Flyer", "XDE60", 2019, "Cummins L9", "BAE Systems", 1000000, "R5", true, 120, false, true));
         bus.add(new Bus("Nova Bus", "LFS", 2008, "Cummins L9", "Allision B400R", 500000, "402", false, 60, false, false));
+        bus.add(new Bus("Alexander Dennis", "Enviro500", 2015, "Cummins ISL9", "Voith D864.5", 800000, "301", true, 100, false, false));
         model.addAttribute("busses", bus);
         return "bus/previewTable";
     }
